79a3d2b0e853188672185c9b51f37d0fe0517ae7
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-09-28 23:08:01 -0400

    checkpoint 5: complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,20 +3,22 @@
       $locationProvider
          	.html5Mode({
             enabled: true,
-            requiredBase: false;
+            requiredBase: false
       })
 			 $stateProvider
          	.state('landing', {
             url: '/',
+            controller: 'LandingCtrl as landing',
             templateUrl: '/templates/landing.html'
        })
         	 .state('album', {
             url: '/album',
             templateUrl: '/templates/album.html'
        })
-			 
+
 			 		.state('collection', {
 					 url: '/collection',
+           controller: 'CollectionCtrl as collection',
 					 templateUrl: '/templates/colleciton.html'
 			 });
     }
