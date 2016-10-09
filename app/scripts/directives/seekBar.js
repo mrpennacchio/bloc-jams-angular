7b204e4a2d68b97f4115280be7fea178d396dc1d
Anthony <Anthony@Joyces-MacBook-Pro.local>
2016-10-09 09:29:01 -0400

    checkpoint-10: complete

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,65 +1,110 @@
 (function() {
-  function seekBar ($document) {
+  function seekBar($document) {
 
-  var calculatePercent = function(seekBar, event) {
-    var offsetX = event.pageX - seekBar.offset().left;
-    var seekBarWidth = seekBar.width();
-    var offsetXPercent = offsetX / seekBarWidth;
-    offsetXPercent = Math.max(0, offsetXPercent);
-    offsetXPercent = Math.min(1, offsetXPercent);
-    return offsetXPercent;
-  };
+    /**
+    * @function calculatePercent
+    * @desc Calculates percentage of seek bar length
+    * @params {Object} seekBar, {Object} event
+    */
+    var calculatePercent = function(seekBar, event) {
+      var offsetX = event.pageX - seekBar.offset().left;
+      var seekBarWidth = seekBar.width();
+      var offsetXPercent = offsetX / seekBarWidth;
+      offsetXPercent = Math.max(0, offsetXPercent);
+      offsetXPercent = Math.min(1, offsetXPercent);
+      return offsetXPercent;
+    };
 
     return {
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: { },
+      scope: { onChange: '&' },
+      //updates DOM
       link: function(scope, element, attributes) {
-        scope.value = 0;
-        scope.max = 100;
+      // bulk of directive logic is placed here and is returned
+      scope.value = 0;
+      scope.max = 100;
 
-        var seekBar = $(element);
+      var seekBar = $(element);
 
-        var percentString = function () {
-          var value = scope.value;
-          var max = scope.max;
-          var percent = value / max * 100;
-          return percent + "%";
-        };
+      attributes.$observe('value', function(newValue) {
+        scope.value = newValue;
+      });
 
-        scope.fillStyle = function(){
-          return {width: percentString()};
-        };
+      attributes.$observe('max', function(newValue) {
+        scope.max = newValue;
+      });
 
-        scope.thumbStyle = function() {
-          return {left: percentString()};
-        };
+      /**
+      * @function percentString
+      * @desc Converts percentage number to string type
+      */
+      var percentString = function () {
+        var value = scope.value;
+        var max = scope.max;
+        var percent = value / max * 100;
+        return percent + "%";
+      };
 
-        scope.onClickSeekBar = function(event) {
+      /**
+      * @function fillStyle
+      * @desc Returns width of bar based on percent
+      */
+      scope.fillStyle = function() {
+        return {width: percentString()};
+      };
+
+      /**
+      * @function thumbStyle
+      * @desc Updates position of thumb
+      */
+      scope.thumbStyle = function() {
+        return {left: percentString()};
+      };
+
+      /**
+      * @function onClickSeekBar
+      * @desc Updates seek bar value based on bar width and location of click
+      */
+      scope.onClickSeekBar = function(event) {
+        var percent = calculatePercent(seekBar, event);
+        scope.value = percent * scope.max;
+        notifyOnChange(scope.value);
+      };
+
+      /**
+      * @function trackThumb
+      * @desc Continuously applies change to seek bar value when thumb is dragged
+      */
+      scope.trackThumb = function() {
+        $document.bind('mousemove.thumb', function(event) {
           var percent = calculatePercent(seekBar, event);
-          scope.value = percent * scope.max;
-        };
-
-        scope.trackThumb = function() {
-          $document.bind('mousemove.thumb', function(event){
-            var percent = calculatePercent(seekBar, event);
-            scope.$apply(function() {
-              scope.value = percent * scope.max;
-            });
+          scope.$apply(function() {
+            scope.value = percent * scope.max;
+            notifyOnChange(scope.value);
           });
+        });
 
-          $document.bind('mouseup.thumb', function(){
-            $document.unbind('mousemove.thumb');
-            $document.unbind('mouseup.thumb');
-          });
-        };
+        $document.bind('mouseup.thumb', function() {
+          $document.unbind('mousemove.thumb');
+          $document.unbind('mouseup.thumb');
+        });
 
-      }
+      }; // end of trackThumb method
+
+      var notifyOnChange = function(newValue) {
+        if (typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
+      };
+
+      } // end of link method
     };
+
   }
 
-    angular
-      .module('blocJams')
-      .directive('seekBar', ['$document', seekBar]);
+  angular
+    .module('blocJams')
+    .directive('seekBar', ['$document', seekBar]);
 })();
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
 
         /**
@@ -29,6 +29,13 @@
           formats: ['mp3'],
           preload: true
       });
+
+      currentBuzzObject.bind('timeupdate', function() {
+        $rootScope.$apply(function() {
+           SongPlayer.currentTime = currentBuzzObject.getTime();
+       });
+     });
+
         SongPlayer.currentSong = song;
       };
 
@@ -56,6 +63,11 @@
       };
 
       SongPlayer.currentSong = null;
+      /**
+       * @desc Current playback time (in seconds) of currently playing song
+       * @type {Number}
+       */
+       SongPlayer.currentTime = null;
 
       /**
       * @function .play
@@ -110,10 +122,21 @@
           }
         };
 
+        /**
+         * @function setCurrentTime
+         * @desc Set current time (in seconds) of currently playing song
+         * @param {Number} time
+         */
+         SongPlayer.setCurrentTime = function(time) {
+             if (currentBuzzObject) {
+                 currentBuzzObject.setTime(time);
+             }
+         };
+
         return SongPlayer;
      }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
